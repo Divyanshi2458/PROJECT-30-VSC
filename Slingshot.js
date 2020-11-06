1df68afa2130bdc5e6e70d651d9bcd248771a33b
Divyanshi2458 <70753678+Divyanshi2458@users.noreply.github.com>
2020-11-06 23:06:49 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,35 @@
+
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        
+        this.pointB = pointB
+        this.Sling = Constraint.create(options);
+        World.add(world, this.Sling);
+    }
+    attach(body){
+        this.Sling.bodyA = body;
+    }
+
+    fly(){
+        this.Sling.bodyA = null;
+    }
+
+    
+
+    
+    display(){
+
+       
+        if(this.Sling.bodyA){
+            var pointA = this.Sling.bodyA.position;
+            var pointB = this.pointB;
+           line (pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+    
+}}
